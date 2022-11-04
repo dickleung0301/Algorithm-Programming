b0cf5646142e5d40f80cdbd59aa23ba71c289ade
My Name <85300097+dickleung0301@users.noreply.github.com>
2022-11-04 08:20:51 +0800

    Task1.2.py completed

--- lec_02/Task1.2.py
+++ lec_02/Task1.2.py
@@ -2,14 +2,16 @@ import sys
 
 args = sys.argv
 
-def Task1_1(args):
+def Task1_2(args):
     if len(args) != 4:
         print(False)
-    elif (len(args[1]) > 1) or (len(args[2]) > 1) or (len(args[3]) > 1):
+    elif (len(args[1]) > 1) or (len(args[2]) > 1) or len(args[2]) > 1: # check str first or flow into other check will ereor
         print(False)
-    elif (ord(args[1]) > 9) or (ord(args[2]) > 9) or (ord(args[3]) > 9): # ascii table
+    elif (ord(args[1]) > 57) or (ord(args[2]) > 57) or (ord(args[3]) > 57) or (ord(args[1]) < 48) or (ord(args[2]) < 48) or (ord(args[3]) < 48): # ascii table
         print(False)
-    elif (int(args[1]) % int(args[2]) == 0) or (int(args[2]) % int(args[1]) == 0):
+    elif (int(args[3]) >= (int(args[1]) + int(args[2]))) or (int(args[1]) >= (int(args[2]) + int(args[3]))) or (int(args[2]) >= (int(args[1]) + int(args[3]))):
         print(True)
+    else:
+        print(False)
 
-Task1_1(args)
+Task1_2(args)
