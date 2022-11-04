c819e98bd4fd2af8eb9c40322f4ff4c00f77be0b
My Name <85300097+dickleung0301@users.noreply.github.com>
2022-11-04 08:46:56 +0800

    revised Task1.1.py Task1.2.py

--- lec_02/Task1.1.py
+++ lec_02/Task1.1.py
@@ -3,11 +3,15 @@ import sys
 args = sys.argv
 
 def Task1_1(args):
+    for i in range(len(args[1])):
+        if (ord(args[1][i]) > 57) or (ord(args[1][i]) < 48): # check str first or flow into other check will ereor
+            print(False)
+            return
+    for i in range(len(args[2])):
+        if (ord(args[2][i]) > 57) or (ord(args[2][i]) < 48): # check str first or flow into other check will ereor
+            print(False)
+            return
     if len(args) != 3:
-        print(False)
-    elif (len(args[1]) > 1) or (len(args[2]) > 1):
-        print(False)
-    elif (ord(args[1]) > 57) or (ord(args[2]) > 57) or (ord(args[1]) < 48) or (ord(args[2]) < 48): # ascii table
         print(False)
     elif (int(args[1]) % int(args[2]) == 0) or (int(args[2]) % int(args[1]) == 0):
         print(True)
--- lec_02/Task1.2.py
+++ lec_02/Task1.2.py
@@ -3,11 +3,19 @@ import sys
 args = sys.argv
 
 def Task1_2(args):
+    for i in range(len(args[1])):
+        if (ord(args[1][i]) > 57) or (ord(args[1][i]) < 48): # check str first or flow into other check will ereor
+            print(False)
+            return
+    for i in range(len(args[2])):
+        if (ord(args[2][i]) > 57) or (ord(args[2][i]) < 48): # check str first or flow into other check will ereor
+            print(False)
+            return
+    for i in range(len(args[3])):
+        if (ord(args[3][i]) > 57) or (ord(args[3][i]) < 48): # check str first or flow into other check will ereor
+            print(False)
+            return
     if len(args) != 4:
-        print(False)
-    elif (len(args[1]) > 1) or (len(args[2]) > 1) or len(args[2]) > 1: # check str first or flow into other check will ereor
-        print(False)
-    elif (ord(args[1]) > 57) or (ord(args[2]) > 57) or (ord(args[3]) > 57) or (ord(args[1]) < 48) or (ord(args[2]) < 48) or (ord(args[3]) < 48): # ascii table
         print(False)
     elif (int(args[3]) >= (int(args[1]) + int(args[2]))) or (int(args[1]) >= (int(args[2]) + int(args[3]))) or (int(args[2]) >= (int(args[1]) + int(args[3]))):
         print(True)
