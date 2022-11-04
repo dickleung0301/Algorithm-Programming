059a4a0ba59dc9a216ff1b1f2231db36ebcdf4cd
My Name <85300097+dickleung0301@users.noreply.github.com>
2022-11-04 09:01:55 +0800

    make the code dry

--- lec_02/Task1.1.py
+++ lec_02/Task1.1.py
@@ -2,15 +2,18 @@ import sys
 
 args = sys.argv
 
+def check_str(index): # check str first or flow into other check will ereor
+    for i in range(len(args[index])):
+        if (ord(args[index][i]) > 57) or (ord(args[index][i]) < 48):
+            return True
+
 def Task1_1(args):
-    for i in range(len(args[1])):
-        if (ord(args[1][i]) > 57) or (ord(args[1][i]) < 48): # check str first or flow into other check will ereor
-            print(False)
-            return
-    for i in range(len(args[2])):
-        if (ord(args[2][i]) > 57) or (ord(args[2][i]) < 48): # check str first or flow into other check will ereor
-            print(False)
-            return
+    if check_str(1):
+        print(False)
+        return
+    if check_str(2):
+        print(False)
+        return
     if len(args) != 3:
         print(False)
     elif (int(args[1]) % int(args[2]) == 0) or (int(args[2]) % int(args[1]) == 0):
--- lec_02/Task1.2.py
+++ lec_02/Task1.2.py
@@ -2,19 +2,21 @@ import sys
 
 args = sys.argv
 
+def check_str(index): # check str first or flow into other check will ereor
+    for i in range(len(args[index])):
+        if (ord(args[index][i]) > 57) or (ord(args[index][i]) < 48):
+            return True
+
 def Task1_2(args):
-    for i in range(len(args[1])):
-        if (ord(args[1][i]) > 57) or (ord(args[1][i]) < 48): # check str first or flow into other check will ereor
-            print(False)
-            return
-    for i in range(len(args[2])):
-        if (ord(args[2][i]) > 57) or (ord(args[2][i]) < 48): # check str first or flow into other check will ereor
-            print(False)
-            return
-    for i in range(len(args[3])):
-        if (ord(args[3][i]) > 57) or (ord(args[3][i]) < 48): # check str first or flow into other check will ereor
-            print(False)
-            return
+    if check_str(1):
+        print(False)
+        return
+    if check_str(2):
+        print(False)
+        return
+    if check_str(3):
+        print(False)
+        return
     if len(args) != 4:
         print(False)
     elif (int(args[3]) >= (int(args[1]) + int(args[2]))) or (int(args[1]) >= (int(args[2]) + int(args[3]))) or (int(args[2]) >= (int(args[1]) + int(args[3]))):
