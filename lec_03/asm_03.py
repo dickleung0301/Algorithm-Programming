b093527f6bfc4964a1c8b6194d3a4ca159a3b266
My Name <85300097+dickleung0301@users.noreply.github.com>
2022-11-16 14:07:43 +0100

    updated version

--- lec_03/asm_03.py
+++ lec_03/asm_03.py
@@ -2,18 +2,22 @@
 print('Task 1:')
 class Person:
     def __init__(self,name,age,height):
-        self.name = name
-        self.age = age
-        self.height = height
+        self.__name = name
+        self.__age = age
+        self.__height = height
     def __str__(self):
-        i = str(self.age)
-        j = str(self.height)
-        return(self.name + ' ' + i + ' ' + j)
+        i = str(self.__age)
+        j = str(self.__height)
+        return(self.__name + ' ' + i + ' ' + j)
     def walk(self,meters):
         temp = str(meters)
-        print(self.name + ' walks for ' + temp + ' m!')
+        print(self.__name + ' walks for ' + temp + ' m!')
     def say_hello(self):
         print('Hello!')
+    def get_name(self):
+        return self.__name
+    def get_height(self):
+        return self.__height
 
 p = Person('John',18,180)
 print(p)
@@ -23,22 +27,21 @@ p.say_hello()
 # Task 2
 print('Task 2:')
 class Dog(Person):
-    def __init__(self,name,color,age,owner):
-        self.name = name
-        self.color = color
-        self.age = age
-        self.owner = owner
+    def __init__(self,name,color,age,owner,height):
+        Person.__init__(self,name,age,height)
+        self.__color = color
+        self.__owner = owner
     def bark(self):
         print('bark! bark! bark!')
     def sit(self):
-        print(self.name + ' has sat down!')
+        print(self.get_name() + ' has sat down!')
     def __str__(self):
-        i = str(self.age)
-        return (self.name + ' ' + self.color + ' ' + i + ' ' + self.owner)
+        i = self.get_name()
+        return (self.get_name()+ ' ' + self.__color + ' ' + i + ' ' + self.__owner + ' ' + self.get_height())
 
-d = Dog('doggy','white',1,'Tim')
+d = Dog('doggy','white',1,'Tim','100')
 print(d)
-d.walk(50.0)
+d.walk(30.0)
 d.say_hello()
 d.bark()
 d.sit()
@@ -46,23 +49,23 @@ d.sit()
 # Task 3
 print('Task 3:')
 class Car(Person):
-    distance_counter = 0.0
-    gasoline_consumption = 8
-    gasoline_tank = 50.0
+    __distance_counter = 0.0
+    __gasoline_consumption = 8
+    __gasoline_tank = 50.0
     def __init__(self,title,owner,topSpeed):
-        self.title = title
-        self.owner = owner
-        self.topSpeed = topSpeed
+        self.__title = title
+        self.__owner = owner
+        self.__topSpeed = topSpeed
     def __str__(self):
-        i = str(self.distance_counter)
-        j = str(self.gasoline_consumption)
-        k = str(self.gasoline_tank)
-        return(self.title + ' ' + self.owner + ' ' + i + ' ' + j + ' ' + k + ' ' + self.topSpeed)
+        i = str(self.__distance_counter)
+        j = str(self.__gasoline_consumption)
+        k = str(self.__gasoline_tank)
+        return(self.__title + ' ' + self.__owner + ' ' + i + ' ' + j + ' ' + k + ' ' + self.__topSpeed)
     def move(self,speed,duration):
-        self.distance_counter += speed * duration
-        self.gasoline_tank -= self.gasoline_consumption
+        self.__distance_counter += speed * duration
+        self.__gasoline_tank -= self.__gasoline_consumption
     def refuel(self,liters):
-        self.gasoline_tank += liters
+        self.__gasoline_tank += liters
 
 c = Car('Audi','Me','120km/h')
 print(c)
